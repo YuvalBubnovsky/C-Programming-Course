92d8318e4f769c7af7b8a235884427bd6d5e5156
YuvalBubnovsky <yuval.bubnovsky@gmail.com>
2021-12-08 04:28:04 +0200

    making room for next assignment

--- my_mat.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-#define N 10
-
-void get_matrix(int mat[N][N]);
-void floyd();
-int has_path(int mat[N][N], int i, int j);
-int get_weight(int mat[N][N], int i, int j);
-int min(int a, int b);
